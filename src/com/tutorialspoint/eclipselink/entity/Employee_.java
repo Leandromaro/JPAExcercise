782e41cd1fc05ddd3c6d810b943b657004e9f071
leandro <leandro@leandro-Inspiron-N4010.(none)>
2015-10-12 22:19:59 -0300

    initial commit

---
+++ src/com/tutorialspoint/eclipselink/entity/Employee_.java
@@ -0,0 +1,14 @@
+package com.tutorialspoint.eclipselink.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2015-10-12T22:16:32.815-0300")
+@StaticMetamodel(Employee.class)
+public class Employee_ {
+	public static volatile SingularAttribute<Employee, Integer> eid;
+	public static volatile SingularAttribute<Employee, String> ename;
+	public static volatile SingularAttribute<Employee, Double> salary;
+	public static volatile SingularAttribute<Employee, String> deg;
+}
---
+++ src/com/tutorialspoint/eclipselink/entity/NonTeachingStaff_.java
@@ -0,0 +1,11 @@
+package com.tutorialspoint.eclipselink.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2015-10-12T22:12:31.826-0300")
+@StaticMetamodel(NonTeachingStaff.class)
+public class NonTeachingStaff_ extends Staff_ {
+	public static volatile SingularAttribute<NonTeachingStaff, String> areaexpertise;
+}
